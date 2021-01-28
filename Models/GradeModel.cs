82160d9f16bbbdc12cc4d2e6213395c86eb10b54
Ikemo4 <67703806+Ikemo4@users.noreply.github.com>
2021-01-27 23:03:21 -0700

    Add project files.

---
+++ Models/GradeModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Morris_Isaac_Homework2.Models
+{
+    public class GradeModel
+    {
+        [Required]
+        [Range(0, 100)]
+        public int Asgmt { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int Groupproject { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int Quiz { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int Exam { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int Intex { get; set; }
+    }
+}
---
+++ wwwroot/js/grade.js
@@ -0,0 +1,59 @@
+﻿$("#submitGrade").click( () => {
+    let grade = (parseInt($("#Asgmt").val()) * .50)
+        + (parseInt($("#Groupproject").val()) * .10)
+        + (parseInt($("#Quiz").val()) * .10)
+        + (parseInt($("#Exam").val()) * .20)
+        + (parseInt($("#Intex").val()) * .10);
+    
+    let letter = "";
+    if (grade >= 94)
+    {
+        letter = "A";
+    }
+    else if (grade >= 90)
+    {
+        letter = "A-";
+    }
+    else if (grade >= 87)
+    {
+        letter = "B+";
+    }
+    else if (grade >= 84)
+    {
+        letter = "B";
+    }
+    else if (grade >= 80)
+    {
+        letter = "B-";
+    }
+    else if (grade >= 77)
+    {
+        letter = "C+";
+    }
+    else if (grade >= 74)
+    {
+        letter = "C";
+    }
+    else if (grade >= 70)
+    {
+        letter = "C-"
+    }
+    else if (grade >= 67)
+    {
+        letter = "D+";
+    }
+    else if (grade >= 64)
+    {
+        letter = "D";
+    }
+    else if (grade >= 60)
+    {
+        letter = "D-";
+    }
+    else
+    {
+        letter = "E";
+    }
+
+    alert("Percent: " + grade.toFixed(2) + "\nLetter Grade: " + letter);
+});
